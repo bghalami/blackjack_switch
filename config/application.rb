e0b19826e77cf860e1f3ad5a6ac9a0195d5390fe
bghalami <benjaminghalami@gmail.com>
2018-11-28 22:24:48 -0700

    quick fix, might make api work

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,10 @@ module BlackjackSwitch
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'https://bens-blackjack-switch.herokuapp.com/',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
   end
 end
